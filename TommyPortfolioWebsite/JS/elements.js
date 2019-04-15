801a3bd4dc6d733f5e6d85333fe4fa1c2b7d2d04
Tommy Biard <49169567+tommybiard@users.noreply.github.com>
2019-04-15 00:39:12 -0700

    Add files via upload

---
+++ TommyPortfolioWebsite/JS/elements.js
@@ -0,0 +1,12 @@
+/**
+ * elements.js
+ *
+ * Handles toggling the navigation menu for small screens
+ * and enables tab support for dropdown menus.
+ * Reworked from functionality in Twenty Fifteen https://wordpress.org/themes/twentyfifteen/
+ * Originally built for Popper: https://github.com/mor10/popper/
+ */
+$(function() {
+'use strict'
+
+}
